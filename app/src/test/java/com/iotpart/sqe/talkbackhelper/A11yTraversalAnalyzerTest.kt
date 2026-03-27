168905529ec84ac3a5d304eb42e9603021c889c5
gunwookim0221 <46421151+gunwookim0221@users.noreply.github.com>
2026-03-27 21:59:49 +0900

    Fix redundant empty wrapper exclusion in traversal analyzer

--- app/src/test/java/com/iotpart/sqe/talkbackhelper/A11yTraversalAnalyzerTest.kt
+++ app/src/test/java/com/iotpart/sqe/talkbackhelper/A11yTraversalAnalyzerTest.kt
@@ -9,7 +9,7 @@ class A11yTraversalAnalyzerTest {
 
     @Test
     fun version_isUpdated() {
-        assertEquals("1.3.0", A11yTraversalAnalyzer.VERSION)
+        assertEquals("1.4.3", A11yTraversalAnalyzer.VERSION)
     }
 
     @Test
